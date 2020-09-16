909b8b8c6250a86f70305f14b3a69ecbd0eae890
Joo Hyun Kim <stevekim1109@gmail.com>
2020-09-16 02:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty.";
   }
-  
+  var updatedLine = [];
+  for (i=0, l=line.length; i<)
   return `The line is currently: `
 }
