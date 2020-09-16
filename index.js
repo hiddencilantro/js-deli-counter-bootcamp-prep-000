fe6a647b0ae5c654e58eacd3fac55a1fb06ba5a7
Joo Hyun Kim <stevekim1109@gmail.com>
2020-09-16 02:17:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    console.log(`Currently serving ${katzDeliLine[0]}.`);
+    return `Currently serving ${katzDeliLine[0]}.`);
     katzDeliLine.shift();
   } else {
     console.log("There is nobody waiting to be served!")
